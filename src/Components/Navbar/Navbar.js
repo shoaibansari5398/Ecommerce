257e17e364e9f2b691a59bcdd7336e6512bc3257
Shoaib Akhtar Ansari <shoaib.ansari5398@gmail.com>
2023-06-04 20:52:29 +0530

    bug fix: fix page navigation when searcing product

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,38 +1,53 @@
 import { NavLink } from "react-router-dom";
-import { useState,useEffect,useContext } from "react";
-import "./Navbar.css"
+import { useState, useEffect, useContext } from "react";
+import "./Navbar.css";
 import { ProductContext } from "../../Context/ProductContext";
 import { FiltersContext } from "../../Context/FiltersContext";
-export default function Header(){
+import { useNavigate } from "react-router-dom";
 
-    const [searchText, setSearchText] = useState("");
-    const {state,productDispatch} = useContext(ProductContext);
-    const {filterDispatch} = useContext(FiltersContext);
-    
-    useEffect(()=>{filterDispatch({type:"SEARCH",payload:searchText})},[searchText,filterDispatch]);
+export default function Header() {
+  const [searchText, setSearchText] = useState("");
+  const { state, productDispatch } = useContext(ProductContext);
+  const { filterDispatch } = useContext(FiltersContext);
+  const navigate = useNavigate();
 
-    return(
-            <div className="navbar">
-                <NavLink to="/" className="nav-direction">
-                    <div>Ecomm</div>
-                </NavLink> 
-                <div>
-                <div>
-                    <input className="search-input" placeholder="Search" type="search" name="search" value={searchText}
-                        onChange={(e)=>setSearchText(e.target.value)} />
-                    {/* <button className="search-btn" type="submit">Search</button> */}
-                    {console.log(searchText)}
-                </div>
-                </div>
-                <div className="nav-features">
-                    <ul className="nav-links">
-                        <NavLink to="/login"  className="nav-direction">
-                            <li className="login-btn">Login</li>
-                        </NavLink>
-                        <NavLink to="/wishlist" className="nav-direction"><li>Wishlist</li></NavLink>
-                        <NavLink to="/cart" className="nav-direction"><li>Cart</li></NavLink>
-                    </ul>
-                </div>
-            </div>
-    )
+  useEffect(() => {
+    filterDispatch({ type: "SEARCH", payload: searchText });
+  }, [searchText, filterDispatch]);
+
+  return (
+    <div className="navbar">
+      <NavLink to="/" className="nav-direction">
+        <div>Ecomm</div>
+      </NavLink>
+      <div>
+        <div>
+          <input
+            className="search-input"
+            placeholder="Search"
+            type="search"
+            name="search"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+              e.target.value.trim() !== "" && navigate("/products");
+            }}
+          />
+        </div>
+      </div>
+      <div className="nav-features">
+        <ul className="nav-links">
+          <NavLink to="/login" className="nav-direction">
+            <li className="login-btn">Login</li>
+          </NavLink>
+          <NavLink to="/wishlist" className="nav-direction">
+            <li>Wishlist</li>
+          </NavLink>
+          <NavLink to="/cart" className="nav-direction">
+            <li>Cart</li>
+          </NavLink>
+        </ul>
+      </div>
+    </div>
+  );
 }
